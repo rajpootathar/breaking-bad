39343f5fedfed6630b869204ca1b1be2fb9ea809
rajpootathar <rajpootathar@gmail.com>
2022-03-14 19:06:22 +0500

    Vector icons added

--- App.js
+++ App.js
@@ -3,17 +3,20 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
 import DetailScreen from './src/screens/DetailScreen';
+import {NativeBaseProvider, Box} from 'native-base';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Detail" component={DetailScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Detail" component={DetailScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
 
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,10 +1,60 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+import {
+  VStack,
+  Input,
+  Button,
+  IconButton,
+  Icon,
+  Text,
+  NativeBaseProvider,
+  Center,
+  Box,
+  Divider,
+  Heading,
+} from 'native-base';
+import {Ionicons, MaterialIcons} from 'react-native-vector-icons';
+
+function SearchBar() {
+  return (
+    <VStack
+      my="4"
+      space={5}
+      w="100%"
+      maxW="300px"
+      divider={
+        <Box px="2">
+          <Divider />
+        </Box>
+      }>
+      <VStack w="100%" space={5} alignSelf="center">
+        <Heading fontSize="lg">Cupertino</Heading>
+        <Input
+          placeholder="Search"
+          variant="filled"
+          width="100%"
+          borderRadius="10"
+          py="1"
+          px="2"
+          borderWidth="0"
+          InputLeftElement={
+            <Icon
+              ml="2"
+              size="4"
+              color="gray.400"
+              as={<Ionicons name="ios-search" />}
+            />
+          }
+        />
+      </VStack>
+    </VStack>
+  );
+}
 
 function HomeScreen() {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
+      <SearchBar />
     </View>
   );
 }
